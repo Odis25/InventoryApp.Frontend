651d1a9465c65d3c2ed7b5ea1cbdea42b9d1900e
Odis <tracemode@mail.ru>
2021-12-26 12:56:29 +0300

    Add Year field

--- InventoryApp.Frontend/Models/Device/DeviceDto.cs
+++ InventoryApp.Frontend/Models/Device/DeviceDto.cs
@@ -3,6 +3,7 @@
     public class DeviceDto
     {
         public int Id { get; set; }
+        public int? Year { get; set; }
         public string Name { get; set; }
         public string Model { get; set; }
         public string Manufacturer { get; set; }
